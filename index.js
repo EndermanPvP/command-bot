b9d3021bb14661ffd3593ce3615c70b5b4495ec7
EndermanPvP <100229122+EndermanPvP@users.noreply.github.com>
2022-10-31 09:59:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_PRESENCES", "GUILD_MEMBERS", "GUILD_MESSAGE_REACTIONS"]});
-
-const prefix = '?'; /*The command prefixes.*/
-const token = "Token"; /*Entet your token.*/
+const prefix = '?';
 
   client.on("ready", () => {
-	  console.log("Bot made by EndermanPvP#0001!"/*Do not remove credits.*/, "Bot is online.", "If a error 'node trace deseption' comes up, simply ignore it. It does not effect the performance in any way.")
+	  console.log("Bot made by github.com/EndermanPvP"/*Do not remove credits.*/, "Bot is online.")
   
   })
   
@@ -15,8 +13,8 @@ const token = "Token"; /*Entet your token.*/
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	if (command === 'command'/*The command Trigger e.g. ?command. As shown.*/) {
-		message.channel.send('The command \`?command\` is working!') /*Edit the message.*/
+	if (command === 'command') {
+		message.channel.send('The command \`?command\` is working!')
 	}
 });
-client.login(token)
+client.login("Your Bot Token")
